08c9773d1b314d9c4bc628664ae1b2eb282ed32a
Anna <68747649+AnnaSovpel@users.noreply.github.com>
2021-06-15 23:21:08 +0300

    Update searchform.php

--- searchform.php
+++ searchform.php
@@ -1,5 +1,6 @@
 <?php get_header();?>
     <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' )?>" class="form-group">
+        <i class="fa fa-search"></i>
         <input type="text" placeholder="поиск" class="form-control" value="<?php echo get_search_query() ?>" name="s" id="s" />
     </form>
 <?php get_footer();?>
